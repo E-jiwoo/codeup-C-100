532fafd22d40bd1174bd26351c594c9b24561b3f
E-jiwoo <livei07@naver.com>
2023-06-09 21:46:16 +0900

    1008

---
+++ 1008.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	printf("\u250c\u252c\u2510\n"); //ㅂ+한자키 눌러서 모양을 만든 후 유니코드로 변경해서 사용
+	printf("\u251c\u253c\u2524\n");
+	printf("\u2514\u2534\u2518\n"); 
+	
+	return 0;
+}
