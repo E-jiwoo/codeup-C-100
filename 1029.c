4b59a8b635631f5fafc01cff12bdf4462696d99c
E-jiwoo <livei07@naver.com>
2023-10-02 04:57:14 +0900

    1029

--- 1029.c
+++ 1029.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    double d; //double 데이터형을 사용하면 +- 1.7*10-308 ~ +- 1.7*10308 범위의 실수를 저장할 수 있다
+    double d; //double 데이터형을 사용하면 +-1.7*10-308 ~ +-1.7*10308 범위의 실수를 저장할 수 있다
     scanf("%lf", &d); // double(long float) 형식으로 입력
     printf("%.11lf", d);
 }
