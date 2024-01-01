b798b0f7b31a1f07b6279ce4f15c65d1ba9bf8de
E-jiwoo <livei07@naver.com>
2024-01-01 23:41:56 +0900

    1066

--- 1066.c
+++ 1066.c
@@ -3,6 +3,31 @@
 int main(){
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
+    if(a%2==0){
+        printf("even\n");
+    }
+    else{
+    	printf("odd\n");
+	}
+    if(b%2==0){
+        printf("even\n");
+    }
+    else{
+    	printf("odd\n");
+	}
+    if(c%2==0){
+        printf("even\n");
+    }
+    else{
+    	printf("odd\n");
+	}
+}
+
+/*#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
     
     printf("%s\n", a%2==0 ? "even" : "odd");
     printf("%s\n", b%2==0 ? "even" : "odd");
@@ -10,4 +35,4 @@ int main(){
     
     return 0;
 }
-
+*/
