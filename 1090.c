8a0a16425710db4363e0894b08bb95fcfe38e9d6
E-jiwoo <livei07@naver.com>
2024-01-07 23:36:49 +0900

    1090

---
+++ 1090.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b, c, i;
+    
+    scanf("%lld %lld %lld", &a, &b, &c);
+    for(i=1; i<c; i++){
+        a *= b;
+    }
+    
+    printf("%lld", a);
+    
+    return 0;
+}
